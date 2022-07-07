d61ba0c58e8f0919c3e244a54f9e029b965f02ed
Георгий Бурнаев <burnaev_01@mail.ru>
2022-07-07 22:09:12 +0400

    list page for data, transition to the next page and previous does not work

--- src/App.js
+++ src/App.js
@@ -2,60 +2,99 @@ import './App.css';
 import ListElement from './ListElement';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { render } from 'react-dom';
 
-function App() {
-    const handleCLickPeople = () => {
-        const root = ReactDOM.createRoot(document.getElementById('root'));
-        root.render(
-            <React.StrictMode>
-                <ListElement />
-            </React.StrictMode>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+class App extends React.Component {
+    handleClick(buttonName) {
+        switch (buttonName) {
+            case 'People':
+                root.render(
+                    <React.StrictMode>
+                        <ListElement url="https://swapi.dev/api/people/" />
+                    </React.StrictMode>
+                );
+                break;
+            case 'Starships':
+                root.render(
+                    <React.StrictMode>
+                        <ListElement url="https://swapi.dev/api/starships/" />
+                    </React.StrictMode>
+                );
+                break;
+            case 'Vehicles':
+                root.render(
+                    <React.StrictMode>
+                        <ListElement url="https://swapi.dev/api/vehicles/" />
+                    </React.StrictMode>
+                );
+                break;
+            case 'Planets':
+                root.render(
+                    <React.StrictMode>
+                        <ListElement url="https://swapi.dev/api/planets/" />
+                    </React.StrictMode>
+                );
+                break;
+            case 'Species':
+                root.render(
+                    <React.StrictMode>
+                        <ListElement url="https://swapi.dev/api/species/" />
+                    </React.StrictMode>
+                );
+                break;
+            case 'Films':
+                root.render(
+                    <React.StrictMode>
+                        <ListElement url="https://swapi.dev/api/films/" />
+                    </React.StrictMode>
+                );
+                break;
+            default:
+                break;
+        }
+    }
+    render() {
+        return (
+            <div className='App'>
+                <div className="App-header">
+                    <div className="inner">
+                        <a
+                            className="App-link"
+                            href="https://starwars.ru/"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >Star Wars
+                        </a>
+                        <span id="test"></span>
+                    </div>
+                </div>
+                <div className="App-body">
+                    <tr>
+                        <td>
+                            <button className="App-buttom" onClick={this.handleClick.bind(this, "People")}>People</button>
+                        </td>
+                        <td>
+                            <button className="App-buttom" onClick={this.handleClick.bind(this, "Planets")}>Planets</button>
+                        </td>
+                        <td>
+                            <button className="App-buttom" onClick={this.handleClick.bind(this, "Starships")}>Starships</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <button className="App-buttom" onClick={this.handleClick.bind(this, "Vehicles")}>Vehicles</button>
+                        </td>
+                        <td>
+                            <button className="App-buttom" onClick={this.handleClick.bind(this, "Species")}>Species</button>
+                        </td>
+                        <td>
+                            <button className="App-buttom" onClick={this.handleClick.bind(this, "Films")}>Films</button>
+                        </td>
+                    </tr>
+                </div>
+            </div>
         );
     }
-    const handleCLickStarships = () => {
-        const root = ReactDOM.createRoot(document.getElementById('root'));
-        root.render(
-            <React.StrictMode>
-                <ListElement />
-            </React.StrictMode>
-        );
-    }
-    const handleCLickPlanet = () => {
-        const root = ReactDOM.createRoot(document.getElementById('root'));
-        root.render(
-            <React.StrictMode>
-                <ListElement />
-            </React.StrictMode>
-        );
-    }
-  return (
-    <div className="App">
-          <div className="App-header">
-              <div className="inner">
-                    <a
-                    className="App-link"
-                    href="https://starwars.ru/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    >Star Wars
-                  </a>
-                  <span id="test"></span>
-              </div>
-          </div>
-          <div className="App-body">
-              <tr>
-                    <td>
-                      <button className="App-buttom" onClick={handleCLickPeople}>people</button>
-                    </td>
-                    <td>
-                      <button className="App-buttom" onClick={handleCLickPlanet}>planet</button>
-                    </td>
-                    <td>
-                      <button className="App-buttom" onClick={handleCLickStarships}>starships</button>
-                    </td>
-                </tr>
-        </div>
-    </div>
-  );
 }
 export default App;
--- src/ListElement.js
+++ src/ListElement.js
@@ -1,9 +1,102 @@
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-function ListElement() {
-    return (
-        <div className="App">
-            <button>Hi</button>
-        </div>
-)}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+class ListElement extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: false,
+            isLoaded: false,
+            count: 0,
+            items: [],
+            next: null,
+            previous: null
+        };
+    }
+    componentDidMount() {
+        fetch(this.props.url)
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    this.setState({
+                        isLoaded: true,
+                        count: result.count,
+                        items: result.results,
+                        next: result.next,
+                        previous: result.previous
+                    });
+                },
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+            );
+    }
+
+    handleClick(buttonName) {
+        switch (buttonName) {
+            case 'Next':
+                if (this.state.next != null) {
+                    root.render(
+                        <React.StrictMode>
+                            <ListElement url={this.state.next} />
+                        </React.StrictMode>
+                    );
+                }
+                break;
+            case 'Previous':
+                if (this.state.previous != null) {
+                    root.render(
+                        <React.StrictMode>
+                            <ListElement url={this.state.previous} />
+                        </React.StrictMode>
+                    );
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    render() {
+        const { error, isLoaded, count, items } = this.state;
+        if (error) {
+            return <p> Error {error.message}</p>
+        } else if (!isLoaded) {
+            return <p> Loading... </p>
+        } else {
+            return (
+                <div>
+                    <header>
+                        <tr>
+                            <h1>List</h1>
+                        </tr>
+                        <tr>
+                            <p> Count = {count} </p>
+                        </tr>
+                    </header>
+                    <table align="center" cellSpacing="15">
+                        <td>
+                            <button className="App-buttom" onClick={this.handleClick.bind(this, "Previous")}>Previous</button>
+                        </td>
+                        <td>
+                            {items.map(item =>
+                                <tr key={item.name}>
+                                    <td> <button className="btn">{item.name}</button> </td>
+                                </tr>)
+                            }
+                        </td>
+                        <td>
+                            <button className="App-buttom" onClick={this.handleClick.bind(this, "Next")}>Next</button>
+                        </td>
+                    </table>
+                </div>
+            );
+        }
+    }
+}
+
 export default ListElement;
